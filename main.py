fcb3b6ff9758b5fdf2d06b63758223aeb3b978d6
darcyamadio <>
2021-05-06 17:39:49 +0000

    this is my final code.

---
+++ main.py
@@ -0,0 +1,51 @@
+#question 1 # my codes are commented out.
+# print('What is your first name?')  
+# first_name = input()      #first name is typed
+# print('What is your last name?')
+# last_name = input()       #last name is typed
+# print('Hi', last_name, first_name) #prints name out 
+
+#question 2
+# print('What is your number?')
+# given_number = int(input())
+# if given_number == 0:
+  # print('Zero')
+# elif given_number % 2 == 1: # x divided by 2 and a remainder 1 always = a odd number
+  # print('Odd')
+# else:
+  # print('Even')
+
+#question 3 
+# print('What day is it?') # ask for day number
+# day_given = int(input()) 
+# if day_given in range (0,31 +1): #if num. is in days of january, it stats january than day number
+#   print('January', day_given)
+# elif day_given in range (32, 59 +1):
+#   print('Febuary', day_given - 31) # same thing happens for other months, except I subtract the days form the months before so the number that goes with the month is good.
+# elif day_given in range (60, 90 +1):
+#   print('March', day_given - 59)
+# elif day_given  in range (91, 120 +1):
+#   print('April', day_given - 90)
+# elif day_given in range (121 , 151 +1):
+#   print('May', day_given - 120)
+# elif day_given in range (152,181 +1):
+#   print('June', day_given - 151)
+# elif day_given in range (182, 212 +1):
+#   print('July', day_given - 181)
+# elif day_given in range (213, 243 +1):
+#   print('August', day_given - 212)
+# elif day_given in range (244, 273 +1):
+#   print('September', day_given - 243)
+# elif day_given in range (274, 304 +1):
+#   print('October', day_given - 273)
+# elif day_given in range (305, 334 +1):
+#   print('November', day_given - 304)
+# elif day_given in range (335, 365 +1):
+#   print('December', day_given - 334)
+
+#question 4   # i ran out of time, as question 3 took me                  very long to complete
+n = 5
+for i in range (n, 0, -1):
+  print(i)
+  
+  
